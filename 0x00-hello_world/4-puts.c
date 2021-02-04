a224aad33e4c8300743b29a6f54d8cdaf620a5af
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-02-03 23:53:56 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-/**                                                                             
- * main - Entry point                                                           
- *                                                                              
- * Return: Always 0 (Success)                                                   
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+char str1[52];
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
+puts(str1);
+
+return (0);
 }
