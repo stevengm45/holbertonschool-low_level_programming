08acfab5a630177b2b5d10436a5453ea3bce70c1
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-02-05 00:35:06 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,6 @@
  *
  * Return: Always 0 (Success)
  */
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 int main(void)
 {
 int n;
@@ -16,7 +13,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-  if (n > 5)
+if (n > 5)
 {
 printf("%d and is greater than 5\n", n);
 }
@@ -26,7 +23,7 @@ printf("%d is 0\n", n);
 }
 else
 {
-printf("%d is negative\n", n);
+printf("%d and is less than 6 and not 0\n", n);
 }
 
 return (0);
