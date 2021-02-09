d7633eb8f34eafaa3e9b852eb7385fe4e99720d5
Steven Gonzalez <stevengm45@gmail.com>
2021-02-09 14:59:25 +0000

    lvl c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+
+void print_alphabet()
+{
+  char c = 'a';
+  while ( c <= 'z')
+    {
+      _putchar (c);
+      c++;
+    }
+ _putchar('\n');
+}
