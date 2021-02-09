477753ea2a085f61dfa56df5c2e52429b503e857
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-02-09 11:56:40 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - Prints the alphabet in lowercase.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet()
