8affb796d47bf3a369182cb89b724dd78eed27b6
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-02-10 09:23:13 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 #include <ctype.h>
 /**
- * _islower - Check if the entry is in lower.
- * @c: This is the entry
- *
- * Return: Always 0.
- */
+ *_islower - check if c is lower
+ *@c: is entry point
+ *Return: 0
+ **/
 int _islower(int c)
 {
-if (c <= 'a' && c >= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
