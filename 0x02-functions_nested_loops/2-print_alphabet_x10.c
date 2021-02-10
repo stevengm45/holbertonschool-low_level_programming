c0b02b4bd976c47d069591f254e4eb7e4f3eaec9
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-02-10 10:23:48 -0500

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,14 @@
 void print_alphabet_x10(void)
 {
 int i = 0;
-while (i == 9)
+while (i < 10)
 {
 int j = 97;
 while (j < 122)
 {
-_putchar('\n',j);
+_putchar(j);
 j++;
+_putchar('\n');
 }
 i++;
 }
