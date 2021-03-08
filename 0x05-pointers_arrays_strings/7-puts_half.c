700b720b4a9423690e0233d2bdabcd5fb4d200a5
Steven Gonzalez <stevengm45@gmail.com>
2021-03-08 02:49:53 +0000

    fix betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,21 +7,14 @@
 
 void puts_half(char *str)
 {
-  int i, j;
-
-  for (i = 0; str[i] != '\0'; i++)
-    j = i / 2;
-
-  if ((i % 2) + 1 == 1)
-    j++;
-  for(;j < i; j++)
-    {
-      _putchar(str[j]);
-    }
-  _putchar('\n');
+int i, j;
+for (i = 0; str[i] != '\0'; i++)
+j = i / 2;
+if ((i % 2) + 1 == 1)
+j++;
+for (; j < i; j++)
+{
+_putchar(str[j]);
 }
-
-	  
-  
-  
-
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,23 @@
 #include "holberton.h"
 #include <stdio.h>
-
-
-
+/**
+ * print_array - print n digits of an array
+ * @a: array to print
+ * @n: number of values to print
+ * Description: prints n digits of array
+ * Return: nothing
+ **/
 
 void print_array(int *a, int n)
 {
-  int i = 0;
+int i = 0;
 
-  while (i < n)
-    {
-      printf("%d", a[i]);
-      if(i != n - 1)
-	printf(", ");
-      i++;
-    }
-  printf("\n");
-  
+while (i < n)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+printf(", ");
+i++;
+}
+printf("\n");
 }
