d4254a5679b3679c36b2c158be54286f2c0705d7
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-03-09 18:29:24 -0500

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,13 +35,3 @@ dog_t *new_dog(char *name, float age, char *owner)
   return (dognew);
 
 }
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-  struct dog_t *dog;
-  dog = malloc(sizeof(struc dog_t));
-
-  dog->name =name;
-    dog->age = age;
-    dog->owner = owner;
-}
