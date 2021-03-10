80e60ff09772d0617f9b802d846873e477373e6d
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-03-09 22:25:30 -0500

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,7 +36,7 @@ dognew->owner[j] = owner[j];
 if (dognew == NULL)
 return (NULL);
 
-dognew->age = age;
+/*dognew->age = age;
 return (dognew);
 if (dognew->owner == NULL)
 {
@@ -44,5 +44,5 @@ free(dognew->name);
 free(dognew->owner);
 free(dognew);
 return (NULL);
-}
+}*/
 }
