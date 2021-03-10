a8431ea0dba2d1309ac66546eafaf976fa295aea
stevengm45 <70825814+stevengm45@users.noreply.github.com>
2021-03-09 23:13:33 -0500

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,8 +23,14 @@ for (j = 0; owner[j] != '\0'; j++)
 
 dognew = malloc(sizeof(dog_t));
 dognew->name = malloc((i + 1) * sizeof(char));
-dognew->owner = malloc((j + i) * sizeof(char));
-
+dognew->owner = malloc((j + 1) * sizeof(char));
+if ((dognew == NULL) || (dognew->name == NULL) || (dognew->owner == NULL)
+{
+free(dognew->name);
+free(dognew->owner);
+free(dognew);
+return (NULL);
+}
 for (i = 0; name[i] != '\0'; i++)
 dognew->name[i] = name[i];
 dognew->name[i] = '\0';
