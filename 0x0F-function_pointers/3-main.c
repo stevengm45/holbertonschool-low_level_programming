03979cbf8c5efbc63d73320d484f4ea90252db85
Steven Gonzalez <stevengm45@gmail.com>
2021-03-13 04:06:39 +0000

    fix betty style

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 int a, b, c;
 int (*operation)(int, int);
-if(argc !=  4 || argv[2][1] != 0)
+if (argc !=  4 || argv[2][1] != 0)
 {
 printf("Error\n");
 exit(98);
@@ -19,6 +19,6 @@ a = atoi(argv[1]);
 b = atoi(argv[3]);
 operation = get_op_func(argv[2]);
 c = operation(a, b);
-printf("%d\n", c);  
+printf("%d\n", c);
 return (0);
 }
