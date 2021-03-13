210ff7ff9d309a8665bb0662d51e17d9863b640d
Steven Gonzalez <stevengm45@gmail.com>
2021-03-13 04:24:51 +0000

    value variadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,10 +7,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-unsigned int i;
 
 va_list print;
+unsigned int i = 0;
 
- va_start(print, n);
+va_start(print, n);
+
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(print, int));
+if (separator != NULL && i < n - 1)
+printf("%s", separator);
+}
+printf("\n");
 va_end(print);
 }
