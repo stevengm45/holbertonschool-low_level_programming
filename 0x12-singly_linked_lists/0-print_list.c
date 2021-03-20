77e44d5533baca284cdbd4c5da0b2ca289c2f1ee
Steven Gonzalez <70825814+stevengm45@users.noreply.github.com>
2021-03-19 23:36:57 -0500

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,21 +5,21 @@
  * Return: lists lenght
  *
  */
-
 size_t print_list(const list_t *h)
 {
-	int i;
+unsigned int i;
 
-	if (h == NULL)
-		return (0);
-	for (i = 1; h->next != NULL; i++)
-	{
-		if (h->str == NULL)
-			printf("[%u] %s\n", h->len, "(nil)");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-	}
-	printf("[%u] %s\n", h->len, h->str);
-	return (i);
+for (i = 0; !h->next; i++)
+{
+if (h->str == NULL)
+{
+printf("[%u] %s\n", h->len, "(nil)");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+h = h->next;
+}
+return (i);
 }
