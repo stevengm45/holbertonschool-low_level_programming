859f6896d696843cba8ff41326197fd6f82e3bd1
Steven Gonzalez <stevengm45@gmail.com>
2021-03-20 04:18:20 +0000

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,12 +7,13 @@
  */
 
 
-int size_t print_list(const list_t *h)
+size_t print_list(const list_t *h)
 {
-int i;
-for (i = 0; !h; i++)
+unsigned int i;
+
+ for (i = 0; !h; i++)
 {
-if (!h->str)
+if (h->str == NULL)
 {
 printf("[0] (nil)\n");
 }
