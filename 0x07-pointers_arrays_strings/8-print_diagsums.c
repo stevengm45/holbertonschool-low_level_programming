287dc40e23d645539af60e11c0cd093d00b95259
Steven Gonzalez <stevengm45@gmail.com>
2021-04-16 20:43:02 +0000

    betty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,17 +5,15 @@
  * @a: the matrix
  * @size: column size
  */
-
-
 void print_diagsums(int *a, int size)
 {
 	int i = 0, num1 = 0, num2 = 0;
 
 	while (i < size)
-		{
-			num1 = num1 + a[i + (i * size)];
-			num2 = num2 + a[(size * (i + 1)) - (i + 1)];
-			i++;
-		}
+	{
+		num1 = num1 + a[i + (i * size)];
+		num2 = num2 + a[(size * (i + 1)) - (i + 1)];
+		i++;
+	}
 	printf("%d, %d\n", num1, num2);
 }
