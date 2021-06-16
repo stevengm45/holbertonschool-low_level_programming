7630251c06f3b67eb51aec82b5bedb858d487010
stevengm45 <stevengm45@gmail.com>
2021-06-16 18:49:10 +0000

    fix betty

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,13 @@
-#ifndef HASH_TABLE
-#define HASH_TABLE
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /* For dynamic memory*/
+#include <stdio.h> /* Macros */
 #include <string.h>
+
+
+
+
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -20,6 +24,7 @@ typedef struct hash_node_s
 	struct hash_node_s *next;
 } hash_node_t;
 
+
 /**
  * struct hash_table_s - Hash table data structure
  *
@@ -34,7 +39,16 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* Hash table */
+
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 
-#endif
+
+#endif /* HASH_TABLES_H */
