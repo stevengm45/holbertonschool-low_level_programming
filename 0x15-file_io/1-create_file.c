2aa1c4ac92d68ab783979274e1bc956daf235588
usuario <usuario@usuario-pc>
2021-06-22 20:21:06 -0500

    fix style in file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,26 +1,35 @@
 #include "holberton.h"
+
 /**
  * create_file -function that creates a file.
  * @filename : filename
  * @text_content : text
  * Return: 1 on success, -1 failed
  */
-
 int create_file(const char *filename, char *text_content)
 {
-int fn, i;
+	int file = 0, len = 0;
 
-if (filename == NULL)
-return (-1);
+	if (!filename)
+	{
+		return (-1);
+	}
 
-if (text_content)
-for (i = 0; text_content[i]; i++)
+	if (text_content)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+	}
 
-fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fn == -1)
-return (-1);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-write(fn, text_content, i);
-close(fn);
-return (1);
+	if (file < 0)
+	{
+		return (-1);
+	}
+	write(file, text_content, len);
+
+	return (1);
 }
