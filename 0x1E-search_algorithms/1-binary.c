2aad7127316f9662845f586c9f4d28a2b06d41fe
stevengm45 <stevengm45@gmail.com>
2021-09-01 20:30:21 -0500

    fix in word search and betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,13 @@
 #include "search_algos.h"
 
-
+/**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the index where value is located or -1 if array is NULL or no match
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
@@ -8,35 +15,27 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array)
 	{
-		while ( l <= m)
+		while (l <= m)
 		{
 			p = (l + m) / 2;
-			printf("Seraching in array: ");
+
+			printf("Searching in array: ");
 			for (i = l; i <= m; i++)
 			{
-				if(i != m)
-				{
+				if (i != m)
 					printf("%d, ", array[i]);
-				}
 				else
-				{
 					printf("%d", array[i]);
-				}
 			}
+
 			printf("\n");
 
 			if (array[p] < value)
-			{
 				l = p + 1;
-			}
 			else if (array[p] > value)
-			{
 				m = p - 1;
-			}
 			else
-			{
 				return (p);
-			}
 		}
 	}
 	return (-1);
