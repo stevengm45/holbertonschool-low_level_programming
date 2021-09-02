0704a7aec4a13e5fda8117656c9a964a3e6b5c01
stevengm45 <stevengm45@gmail.com>
2021-09-01 19:36:44 -0500

    fix in style betty, add comments and space in line 22

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - Search for a value in an array using linear algorithm.
+ * @array: Array of nums to check where the value is.
+ * @size: Size of the array.
+ * @value: Value to search for.
+ * Return: -1 if value doesn't exist in array, else, the index where value is.
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	unsigned int i;
@@ -11,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 		{
 			tmp = array[i];
 			printf("Value checked array[%d] = [%d]\n", i, tmp);
-			if(tmp == value)
+			if (tmp == value)
 				return (i);
 		}
 	}
